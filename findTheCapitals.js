f795cf80ed0f519583434b24bb33c70bde121c9c
Blanca Sanchez <blancayllen@gmail.com>
2017-04-05 15:16:19 -0700

    findTheCapitals

---
+++ findTheCapitals.js
@@ -0,0 +1,18 @@
+/*Write a function that takes a single string (word) as argument. 
+The function must return an ordered list containing the indexes of all 
+capital letters in the string.
+
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+*/
+
+
+
+var capitals = function (word) {
+  var list = []
+	for(var i = 0; i < word.length; i++){
+       if(word[i] === word[i].toUpperCase()){
+          list.push(i);
+       }
+  }
+  return list;
+};
