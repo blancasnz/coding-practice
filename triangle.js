bd6899115aadcb6fc1ba6628654eeeac153fb476
Blanca Sanchez <blancayllen@gmail.com>
2017-04-12 22:31:07 -0700

    triangle code

--- triangle.js
+++ triangle.js
@@ -1,1 +1,15 @@
-Triangle
+/*
+Make a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+var tri = "";
+for (var i = 1; i <= 7; i++){
+  console.log(tri += "#");
+} 
