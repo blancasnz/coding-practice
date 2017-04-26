99cd240d3edeee490a23baff25b43c3a8b94e430
Blanca Sanchez <blancayllen@gmail.com>
2017-04-25 22:16:30 -0700

    to work if Uppercase letter is found first

--- firstLowercaseAndUppercase.js
+++ firstLowercaseAndUppercase.js
@@ -11,12 +11,13 @@ output -> j
 var lowerAndUpper = function(input){
 	for (var i = 0; i < input.length; i++){
 		for ( j = i + 1; j < input.length; j++){
-			if (input[j] === input[i].toUpperCase()){
+			if (input[i] === input[i].toLowerCase() && input[j] === input[i].toUpperCase()){
 				return input[i];
-			}
-		}
-	}
+            } else if(input[i] === input[i].toUpperCase() && input[j] === input[i].toLowerCase()){
+                return input[i];
+              }
+			
+		 }
+	 }
   return "no matches";
 }
-
-console.log(lowerAndUpper(""));
