e0a4030cf08c36897182d0b335405bf3fbd8af1f
Blanca Sanchez <blancayllen@gmail.com>
2017-04-25 22:19:37 -0700

    cleaned up indentation

--- firstLowercaseAndUppercase.js
+++ firstLowercaseAndUppercase.js
@@ -9,15 +9,15 @@ output -> j
 */
 
 var lowerAndUpper = function(input){
-	for (var i = 0; i < input.length; i++){
-		for ( j = i + 1; j < input.length; j++){
-			if (input[i] === input[i].toLowerCase() && input[j] === input[i].toUpperCase()){
-				return input[i];
+    for (var i = 0; i < input.length; i++){
+        for ( j = i + 1; j < input.length; j++){
+            if (input[i] === input[i].toLowerCase() && input[j] === input[i].toUpperCase()){
+                return input[i];
             } else if(input[i] === input[i].toUpperCase() && input[j] === input[i].toLowerCase()){
                 return input[i];
               }
-			
-		 }
-	 }
+            
+         }
+     }
   return "no matches";
 }
